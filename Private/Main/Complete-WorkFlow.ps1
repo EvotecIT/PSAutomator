99cc1aa5f0f2fea1a5a3d035d66f7c7f1d269ba2
PrzemyslawKlys <pklys@evotec.pl>
2018-10-25 19:07:46 +0200

    Update

--- Private/Main/Complete-WorkFlow.ps1
+++ Private/Main/Complete-WorkFlow.ps1
@@ -75,64 +75,64 @@ Function Complete-WorkFlow {
                 foreach ($User in $Object.ProcessingData.Users) {
                     $Result = switch ( $Action.Action ) {
                         AccountAddGroupsSpecific {
-                            $CommandOutput = Add-ADUserGroups -User $User -Groups $Action.Value -FieldSearch 'Name' -WhatIf:$Action.WhatIf
+                            $CommandOutput = Add-WinADUserGroups -User $User -Groups $Action.Value -FieldSearch 'Name' -WhatIf:$Action.WhatIf
                             Out-ActionStatus -CommandOutput $CommandOutput -User $User -Name $Action.Name -WhatIf:$Action.WhatIf
                         }
                         AccountDisable {
-                            $CommandOutput = Set-ADUserStatus -User $User -Option Disable -WhatIf:$Action.WhatIf
+                            $CommandOutput = Set-WinADUserStatus -User $User -Option Disable -WhatIf:$Action.WhatIf
                             Out-ActionStatus -CommandOutput $CommandOutput -User $User -Name $Action.Name -WhatIf:$Action.WhatIf
                         }
                         AccountEnable {
-                            $CommandOutput = Set-ADUserStatus -User $User -Option Enable -WhatIf:$Action.WhatIf
+                            $CommandOutput = Set-WinADUserStatus -User $User -Option Enable -WhatIf:$Action.WhatIf
                             Out-ActionStatus -CommandOutput $CommandOutput -User $User -Name $Action.Name -WhatIf:$Action.WhatIf
                         }
                         AccountHideInGAL {
-                            $CommandOutput = Set-ADUserSettingGAL -User $User -Option Hide -WhatIf:$Action.WhatIf
+                            $CommandOutput = Set-WinADUserSettingGAL -User $User -Option Hide -WhatIf:$Action.WhatIf
                             Out-ActionStatus -CommandOutput $CommandOutput -User $User -Name $Action.Name -WhatIf:$Action.WhatIf
                         }
                         AccountShowInGAL {
-                            $CommandOutput = Set-ADUserSettingGAL -User $User -Option Show -WhatIf:$Action.WhatIf
+                            $CommandOutput = Set-WinADUserSettingGAL -User $User -Option Show -WhatIf:$Action.WhatIf
                             Out-ActionStatus -CommandOutput $CommandOutput -User $User -Name $Action.Name -WhatIf:$Action.WhatIf
                         }
                         AccountRemoveGroupsAll {
-                            $CommandOutput = Remove-ADUserGroups -User $User -All -WhatIf:$Action.WhatIf
+                            $CommandOutput = Remove-WinADUserGroups -User $User -All -WhatIf:$Action.WhatIf
                             Out-ActionStatus -CommandOutput $CommandOutput -User $User -Name $Action.Name -WhatIf:$Action.WhatIf
                         }
                         AccountRemoveGroupsSecurity {
-                            $CommandOutput = Remove-ADUserGroups -User $User -GroupCategory Security -WhatIf:$Action.WhatIf
+                            $CommandOutput = Remove-WinADUserGroups -User $User -GroupCategory Security -WhatIf:$Action.WhatIf
                             Out-ActionStatus -CommandOutput $CommandOutput -User $User -Name $Action.Name -WhatIf:$Action.WhatIf
                         }
                         AccountRemoveGroupsDistribution {
-                            $CommandOutput = Remove-ADUserGroups -User $User -GroupCategory Distribution -WhatIf:$Action.WhatIf
+                            $CommandOutput = Remove-WinADUserGroups -User $User -GroupCategory Distribution -WhatIf:$Action.WhatIf
                             Out-ActionStatus -CommandOutput $CommandOutput -User $User -Name $Action.Name -WhatIf:$Action.WhatIf
                         }
                         AccountRemoveGroupsDomainLocal {
-                            $CommandOutput = Remove-ADUserGroups -User $User -GroupScope DomainLocal -WhatIf:$Action.WhatIf
+                            $CommandOutput = Remove-WinADUserGroups -User $User -GroupScope DomainLocal -WhatIf:$Action.WhatIf
                             Out-ActionStatus -CommandOutput $CommandOutput -User $User -Name $Action.Name -WhatIf:$Action.WhatIf
                         }
                         AccountRemoveGroupsGlobal {
-                            $CommandOutput = Remove-ADUserGroups -User $User -GroupScope Global -WhatIf:$Action.WhatIf
+                            $CommandOutput = Remove-WinADUserGroups -User $User -GroupScope Global -WhatIf:$Action.WhatIf
                             Out-ActionStatus -CommandOutput $CommandOutput -User $User -Name $Action.Name -WhatIf:$Action.WhatIf
                         }
                         AccountRemoveGroupsUniversal {
-                            $CommandOutput = Remove-ADUserGroups -User $User -GroupScope Universal -WhatIf:$Action.WhatIf
+                            $CommandOutput = Remove-WinADUserGroups -User $User -GroupScope Universal -WhatIf:$Action.WhatIf
                             Out-ActionStatus -CommandOutput $CommandOutput -User $User -Name $Action.Name -WhatIf:$Action.WhatIf
                         }
                         AccountRemoveGroupsSpecific {
-                            $CommandOutput = Remove-ADUserGroups -User $User -Groups $Action.Value -WhatIf:$Action.WhatIf
+                            $CommandOutput = Remove-WinADUserGroups -User $User -Groups $Action.Value -WhatIf:$Action.WhatIf
                             Out-ActionStatus -CommandOutput $CommandOutput -User $User -Name $Action.Name -WhatIf:$Action.WhatIf
                         }
                         AccountRename {
                             if ($Action.Value.Action -eq 'AddText') {
-                                $CommandOutput = Set-ADUserName -User $User -Option $Action.Value.Where -TextToAdd $Action.Value.Text -Fields $Action.Value.Fields -WhatIf:$Action.WhatIf
+                                $CommandOutput = Set-WinADUserFields -User $User -Option $Action.Value.Where -TextToAdd $Action.Value.Text -Fields $Action.Value.Fields -WhatIf:$Action.WhatIf
                                 Out-ActionStatus -CommandOutput $CommandOutput -User $User -Name $Action.Name -WhatIf:$Action.WhatIf
                             } elseif ($Action.Value.Action -eq 'RemoveText') {
-                                $CommandOutput = Set-ADUserName -User $User -TextToRemove $Action.Value.Text -Fields $Action.Value.Fields -WhatIf:$Action.WhatIf
+                                $CommandOutput = Set-WinADUserFields -User $User -TextToRemove $Action.Value.Text -Fields $Action.Value.Fields -WhatIf:$Action.WhatIf
                                 Out-ActionStatus -CommandOutput $CommandOutput -User $User -Name $Action.Name -WhatIf:$Action.WhatIf
                             }
                         }
                         AccountSnapshot {
-                            $CommandOutput = Get-ADUserSnapshot -User $User -XmlPath $Action.Value -WhatIf:$Action.WhatIf
+                            $CommandOutput = Get-WinADUserSnapshot -User $User -XmlPath $Action.Value -WhatIf:$Action.WhatIf
                             Out-ActionStatus -CommandOutput $CommandOutput -User $User -Name $Action.Name -WhatIf:$Action.WhatIf
                         }
                     }
