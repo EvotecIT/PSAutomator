b5b1ce8b19edef5c9ce8d6fc418dfebbf9bf4d9b
PrzemyslawKlys <pklys@evotec.pl>
2018-11-12 23:46:28 +0100

    Updated Azure Example

--- Examples/RunBook.UsersAzure.ps1
+++ Examples/RunBook.UsersAzure.ps1
@@ -4,5 +4,6 @@ Import-Module PSSharedGoods #-Force
 
 Service -Name 'Active Directory Offboarding' -Status Enable -ConfigurationPath 'C:\Support\GitHub\PSAutomator\Examples\MyConfiguration1.xml' {
     Trigger -Name 'Synchronized Users from AD' -UserAzureAD Synchronized |
+        Ignore -Name 'Ignore Synchronization Account' -Ignore MatchingFields -Value @{ Field = 'UserPrincipalName'; Operator = 'notlike'; Value = 'Sync_ADConnect*' } |
         Action -Name 'Disable Synchronized Users' -AzureActiveDirectory AccountDisable -WhatIf
 }
